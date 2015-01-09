bde7df619f9ea003c19dceb487bc1886079fef95
Eric Wardner <ewardner22@gmail.com>
2015-01-09 00:55:56 -0800

    insert tail works

--- list.c
+++ list.c
@@ -16,28 +16,44 @@ node* list_insert_head(node* headPtr, char* data){
 
 node* list_insert_tail(node* headPtr, char* data){
         node* newNode;
+	node* tempNode;
+	tempNode = headPtr;
         newNode = (node*)malloc(sizeof(node));
         newNode->data = data;
 	newNode->next = NULL;
-	//recursion to find end of list
+	//find end of list
 	if(headPtr == NULL){
 		return newNode;
 	}
 	else{
-		while(headPtr != NULL){
-			headPtr = headPtr->next;
+		while(newNode->next != NULL){
+			tempNode = headPtr->next;
 		}
-		headPtr->next = newNode;
+		tempNode->next = newNode;
+		return headPtr;
 	}
 
 }
 
+void list_print(node* headPtr){
+	node* currNode;
+	currNode = headPtr;
+	while(currNode != NULL){
+		printf("%s\n",currNode->data);
+		currNode = currNode->next;
+	}
+}
+
 int main(void){
 	node* list;
-	list->data = NULL;
+	list = NULL;
 	
 	char* one = "one";
+	char* two = "two";
 
-	list = list_insert_tail(list, one);	
+	list = list_insert_tail(list, one);
+	list = list_insert_tail(list, two);
+		
+	list_print(list);
 
 }
